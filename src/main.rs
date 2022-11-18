af130294faa392eeef21ff17eaa8a0ad7734ba31
Jalil David Salamé Messina <jalil.salame@tum.de>
2022-11-18 23:21:36 +0100

    feat: Added simple List and Match API endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,51 @@
-use axum::{extract::Path, routing::get, Router};
-use serde::Deserialize;
+use std::sync::{Arc, RwLock};
 
-#[derive(Debug, Deserialize)]
+use axum::{extract::Path, http::StatusCode, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Clone, Serialize, PartialEq, Eq)]
 enum Action {
     Add,
     Del,
     List,
+    Match,
 }
+
+#[derive(Debug, Clone, Serialize)]
+struct Entry {
+    kind: Action,
+    user: String,
+    security: String,
+    side: Side,
+    quantity: u64,
+    price: u64,
+}
+
+#[derive(Debug, Clone, Serialize)]
+enum Side {
+    Buy,
+    Sell,
+}
+
+type State = RwLock<Vec<Entry>>;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/api/:action", get(api_get).post(api_post));
+    let state: Arc<State> = Arc::new(State::default());
+
+    let app = Router::new()
+        .route("/", get(|| async { "To access the API got to /api" }))
+        .route(
+            "/api/:action",
+            get({
+                let state = Arc::clone(&state);
+                move |action| api_get(action, state)
+            })
+            .post({
+                let state = Arc::clone(&state);
+                move |action| api_post(action, state)
+            }),
+        );
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -20,18 +54,37 @@ async fn main() {
         .unwrap();
 }
 
-async fn api_get(Path(action): Path<Action>) {
+async fn api_get(
+    Path(action): Path<Action>,
+    state: Arc<State>,
+) -> Result<Json<Vec<Entry>>, StatusCode> {
     use Action::*;
+
+    let read = state.read().expect("Valid Mutex");
+
     match action {
-        List => todo!("List handler"),
-        Add | Del => todo!("Err message"),
+        List => Ok(Json(read.to_vec())),
+        Match => Ok(Json(
+            read.iter()
+                .filter(|entry| entry.kind == Action::Match)
+                .cloned()
+                .collect(),
+        )),
+        Add | Del => Err(StatusCode::BAD_REQUEST),
     }
 }
 
-async fn api_post(Path(action): Path<Action>) {
+async fn api_post(
+    Path(action): Path<Action>,
+    state: Arc<State>,
+) -> Result<&'static str, StatusCode> {
     use Action::*;
+
+    let _write = state.write().expect("Valid Mutex");
+
     match action {
-        Add | Del => todo!("Handler"),
-        List => todo!("Err message"),
+        Add => todo!(),
+        Del => todo!(),
+        List | Match => Err(StatusCode::BAD_REQUEST),
     }
 }
